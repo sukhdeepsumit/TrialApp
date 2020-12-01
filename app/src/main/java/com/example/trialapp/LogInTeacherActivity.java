d367820598f7c7425c957ffaeaf42b6088b1d27d
sukhdeepsumit <sukhi.aditya.sumit@gmail.com>
2020-12-01 20:03:02 +0530

    Initial commit

---
+++ app/src/main/java/com/example/trialapp/LogInTeacherActivity.java
@@ -0,0 +1,15 @@
+package com.example.trialapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LogInTeacherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in_student);
+
+    }
+}
---
+++ app/src/main/java/com/example/trialapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.trialapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void screen1(View v) {
+
+            Intent intent = new Intent(MainActivity.this, LogInTeacherActivity.class);
+            startActivity(intent);
+
+        }
+    }
+
