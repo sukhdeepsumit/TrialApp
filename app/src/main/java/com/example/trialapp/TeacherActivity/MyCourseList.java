147b3f8ee75adb2f27d72b361c5ea8d2a14916af
SUKHDEEP SINGH <sukhi.aditya.sumit@gmail.com>
2020-12-06 11:28:18 +0530

    Changes in Manifest File

--- app/src/main/java/com/example/trialapp/TeacherActivity/MyCourseList.java
+++ app/src/main/java/com/example/trialapp/TeacherActivity/MyCourseList.java
@@ -3,13 +3,17 @@ package com.example.trialapp.TeacherActivity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.trialapp.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MyCourseList extends AppCompatActivity {
 
     RecyclerView recyclerView;
+    FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +21,13 @@ public class MyCourseList extends AppCompatActivity {
         setContentView(R.layout.activity_my_course_list);
 
         recyclerView = findViewById(R.id.recyclerView);
+        floatingActionButton = findViewById(R.id.addSubject);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MyCourseList.this, SubjectList.class));
+            }
+        });
     }
 }
