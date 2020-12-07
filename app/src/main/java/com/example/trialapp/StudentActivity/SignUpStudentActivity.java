7847366d5b1903a64c26bde9ca51cb42bfbe35c9
SUKHDEEP SINGH <sukhi.aditya.sumit@gmail.com>
2020-12-07 19:19:17 +0530

    Adpter

--- app/src/main/java/com/example/trialapp/StudentActivity/SignUpStudentActivity.java
+++ app/src/main/java/com/example/trialapp/StudentActivity/SignUpStudentActivity.java
@@ -20,6 +20,8 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.Objects;
+
 public class SignUpStudentActivity extends AppCompatActivity {
     Button signUpStudentBtn;
     private FirebaseAuth mAuth;
@@ -113,15 +115,14 @@ public class SignUpStudentActivity extends AppCompatActivity {
                                         Log.i("TAG", "createUserWithEmail:success");
                                         Toast.makeText(SignUpStudentActivity.this, "Registration Success", Toast.LENGTH_SHORT).show();
                                         startActivity(new Intent(SignUpStudentActivity.this, LogInStudentActivity.class));
-                                        progressBar.setVisibility(View.GONE);
 
 
                                     }
                                     else {
                                         // If sign in fails, display a message to the user.
-                                        Toast.makeText(SignUpStudentActivity.this, "Failure" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                                        progressBar.setVisibility(View.GONE);
+                                        Toast.makeText(SignUpStudentActivity.this, "Failure" + Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
                                     }
+                                    progressBar.setVisibility(View.GONE);
                                 }
                             });
                 }
