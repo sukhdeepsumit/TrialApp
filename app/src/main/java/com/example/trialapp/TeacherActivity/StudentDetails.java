bb146a65ad59bc3a4e5fba5fe956e060ffa03503
sukhdeepsumit <sukhi.aditya.sumit@gmail.com>
2020-12-10 03:42:10 +0530

    Navigation bar added

--- app/src/main/java/com/example/trialapp/TeacherActivity/StudentDetails.java
+++ app/src/main/java/com/example/trialapp/TeacherActivity/StudentDetails.java
@@ -1,19 +1,32 @@
 package com.example.trialapp.TeacherActivity;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.trialapp.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 
 public class StudentDetails extends AppCompatActivity {
 
     RecyclerView recyclerView;
     FloatingActionButton add;
+    NavigationView nav;
+    ActionBarDrawerToggle toggle;
+    DrawerLayout drawerLayout;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +41,51 @@ public class StudentDetails extends AppCompatActivity {
                 startActivity(new Intent(StudentDetails.this, StudentAddForm.class));
             }
         });
+
+
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        nav=findViewById(R.id.navMenu);
+        drawerLayout=findViewById(R.id.drawer);
+
+        toggle=new ActionBarDrawerToggle(this,drawerLayout,
+                R.string.open,R.string.close);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        nav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId())
+                {
+                    case R.id.home : {
+                        Toast.makeText(getApplicationContext(), "Home opened", Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+
+                    }
+                    case R.id.myProfile :
+                        Toast.makeText(getApplicationContext(), "My Account opened", Toast.LENGTH_SHORT).show();
+                        drawerLayout.closeDrawer(GravityCompat.START);
+                        break;
+
+
+
+
+                }
+
+
+                return true;
+            }
+        });
+
+
+
+
+
 
 
     }
