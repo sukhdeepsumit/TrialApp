bfdda1ca6e6495f4f2df38c058fb237853f31e59
SUKHDEEP SINGH <sukhi.aditya.sumit@gmail.com>
2020-12-15 21:34:48 +0530

    Comments on TeacherAccountInfo

--- app/src/main/java/com/example/trialapp/TeacherActivity/TeacherAccountInfo.java
+++ app/src/main/java/com/example/trialapp/TeacherActivity/TeacherAccountInfo.java
@@ -32,6 +32,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
+//class about teacher
 public class TeacherAccountInfo extends AppCompatActivity {
 
     String firstName, lastName, contact, email, subject;
